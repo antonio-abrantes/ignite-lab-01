65152edf6716be6d54270f4b03bba054525b675d
antonio-abrantes <antonio_wac@hotmail.com>
2022-06-20 20:44:22 -0300

    final do dia 01

---
+++ README.md
@@ -0,0 +1,5 @@
+## LINKS UTEIS
+
+- https://rseat.in/lab-graphcms
+- https://app.graphcms.com/clone/1d7442bf5a434389904c44d54a041b01?name=Ignite%20Lab%2002
+- [Meu avatar](https://avatars.githubusercontent.com/u/20172965?v=4)
--- event-plataform/src/App.tsx
+++ event-plataform/src/App.tsx
@@ -1,11 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
 import "./styles/global.css";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
   return (
     <div>
-      <h1>App</h1>
+      <ul>
+        {data?.lessons.map((lesson) => {
+          return <li>{lesson.title}</li>;
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
